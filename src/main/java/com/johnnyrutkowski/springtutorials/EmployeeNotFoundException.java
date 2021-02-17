c56a8d2ba21de03b2cc7f9ea38aea46877140846
johnnybuildsit <johnnypr@udel.edu>
2021-02-17 15:58:36 -0500

    CRUD operations on Employee class

---
+++ src/main/java/com/johnnyrutkowski/springtutorials/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.johnnyrutkowski.springtutorials;
+
+class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
---
+++ src/main/java/com/johnnyrutkowski/springtutorials/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.johnnyrutkowski.springtutorials;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
